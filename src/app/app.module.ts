b2ec3bfa71ba2ce69fd2c5f18320b19cdf83efc1
James <james.harrell71@gmail.com>
2017-09-21 00:00:24 -0500

    Fixed bug in onPlayerReady causing compile error.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import {VgControlsModule} from 'videogular2/controls';
 import {VgOverlayPlayModule} from 'videogular2/overlay-play';
 import {VgBufferingModule} from 'videogular2/buffering';
 
-
 @NgModule({
   declarations: [
     AppComponent,
